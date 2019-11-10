2dec041786567ca24d9d79cd01f97e36522bb5a1
Yakov Rogov <rogov@haulmont.com>
2019-11-10 14:11:02 +0400

    added queue class

--- src/torrent-parser.js
+++ src/torrent-parser.js
@@ -3,15 +3,21 @@ const bencode = require('bencode');
 const crypto = require('crypto');
 const BN = require('bn.js');
 
+module.exports.BLOCK_LEN = Math.pow(2, 14);
+
 module.exports.open = (filepath) => {
   return bencode.decode(fs.readFileSync(filepath));
 };
 
-module.exports.size = torrent => {
-  const size = torrent.info.files ?
+function getSize(torrent) {
+  return torrent.info.files ?
     torrent.info.files.map(file => file.length)
       .reduce((a, b) => a + b, 0) :
     torrent.info.length;
+}
+
+module.exports.size = torrent => {
+  const size = getSize(torrent);
 
   return new BN(size.toString(), 10).toBuffer('be', 8);
 };
@@ -19,4 +25,28 @@ module.exports.size = torrent => {
 module.exports.infoHash = torrent => {
   const info = bencode.encode(torrent.info);
   return crypto.createHash('sha1').update(info).digest();
+};
+
+module.exports.pieceLen = (torrent, pieceIndex) => {
+  const totalLength = getSize(torrent);
+  const pieceLength = torrent.info['piece length'];
+
+  const lastPieceLength = totalLength % pieceLength;
+  const lastPieceIndex = Math.floor(totalLength / pieceLength);
+
+  return lastPieceIndex === pieceIndex ? lastPieceLength : pieceLength;
+};
+
+module.exports.blocksPerPiece = (torrent, pieceIndex) => {
+  const pieceLength = this.pieceLen(torrent, pieceIndex);
+  return Math.ceil(pieceLength / this.BLOCK_LEN);
+};
+
+module.exports.blockLen = (torrent, pieceIndex, blockIndex) => {
+  const pieceLength = this.pieceLen(torrent, pieceIndex);
+
+  const lastPieceLength = pieceLength % this.BLOCK_LEN;
+  const lastPieceIndex = Math.floor(pieceLength / this.BLOCK_LEN);
+
+  return blockIndex === lastPieceIndex ? lastPieceLength : this.BLOCK_LEN;
 };
